4776d217356cef8cdc67f5aaa2c6d9bc5e7c05b0
C043 <mariofragnitoph@gmail.com>
2024-06-24 16:24:37 +0200

    Adds rounded img

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -2,7 +2,7 @@ import { Component } from "react";
 
 class ImageComponent extends Component {
   render() {
-    return <img className="mb-5" src={this.props.src} alt={this.props.alt} style={this.props.style} />;
+    return <img className="mb-5 rounded-5" src={this.props.src} alt={this.props.alt} style={this.props.style} />;
   }
 }
 
